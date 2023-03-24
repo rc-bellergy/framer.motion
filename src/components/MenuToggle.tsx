35899a2b13fb855371775c1d05af7919049ce80a
michael <michael@designquest.com.hk>
2023-03-24 22:33:22 +0800

    A Zoom Menu Toggle animation

---
+++ src/components/MenuToggle.tsx
@@ -0,0 +1,74 @@
+import { motion, MotionConfig } from 'framer-motion'
+import styles from '@/components/MenuToggle.module.css'
+
+export default function MenuToggle ({ toggle, isOpen, extendHeight }: any) {
+  const background = {
+    open: (height = extendHeight) => ({
+      clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+      transition: {
+        type: 'spring',
+        stiffness: 80,
+        restDelta: 2
+      }
+    }),
+    closed: {
+      clipPath: 'circle(42px at 30px 30px)',
+      transition: {
+        delay: 0.15,
+        type: 'spring',
+        stiffness: 600,
+        damping: 40
+      }
+    }
+  }
+
+  const Path = (props: any) => (
+    <motion.path
+      fill='transparent'
+      strokeWidth='3'
+      stroke='hsl(0, 0%, 100%)'
+      strokeLinecap='round'
+      {...props}
+    />
+  )
+
+  return (
+    <>
+      <motion.div
+        className={styles.background}
+        variants={background}
+        animate={isOpen ? 'open' : 'closed'}
+      />
+      <button onClick={toggle} className={styles.button}>
+        <motion.svg
+          width='23'
+          height='23'
+          viewBox='0 0 23 23'
+          className='mx-auto'
+          animate={isOpen ? 'open' : 'closed'}
+        >
+          <Path
+            variants={{
+              closed: { d: 'M 2 2.5 L 20 2.5' },
+              open: { d: 'M 3 16.5 L 17 2.5' }
+            }}
+          />
+          <Path
+            d='M 2 9.423 L 20 9.423'
+            variants={{
+              closed: { opacity: 1 },
+              open: { opacity: 0 }
+            }}
+            transition={{ duration: 0.1 }}
+          />
+          <Path
+            variants={{
+              closed: { d: 'M 2 16.346 L 20 16.346' },
+              open: { d: 'M 3 2.5 L 17 16.346' }
+            }}
+          />
+        </motion.svg>
+      </button>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,38 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import { motion } from 'framer-motion'
+import { useState, useRef } from 'react'
+import MenuToggle from '@/components/MenuToggle'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home () {
+  const containerRef = useRef(null)
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <>
       <Head>
-      <title>My First Page</title>
+        <title>My First Page</title>
       </Head>
-      <main className={styles.main}>
-        <h1>Framer-Motion</h1>
-        <h3>TESTING</h3>
-        <motion.dev
-          className={styles.sphare}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+      <main className={styles.main} ref={containerRef}>
+        <MenuToggle
+          toggle={() => toggle()}
+          isOpen={isOpen}
+          extendHeight={300}
         />
+        <section className={styles.description}>
+          <h1>Framer.Motion Testing</h1>
+          <h3>A Zoom Menu Toggle</h3>
+          <a href='https://www.framer.com/motion/examples/#variants'>
+            Reference
+          </a>
+        </section>
       </main>
     </>
   )
