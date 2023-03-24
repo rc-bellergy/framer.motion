33568b3e046cf697f21e290a418345366724c038
michael <michael@designquest.com.hk>
2023-03-24 14:07:43 +0800

    Migrated to TypeScript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
+import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { motion } from 'framer-motion'
-import { Head } from 'next/document'
-
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -9,7 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home () {
   return (
     <>
+      <Head>
       <title>My First Page</title>
+      </Head>
       <main className={styles.main}>
         <h1>Framer-Motion</h1>
         <h3>TESTING</h3>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,jsx}",
-    "./src/components/**/*.{js,jsx}"
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
